5cd6341757a002c269638e133e0a2946424d9e27
Menderes Koc <mndrs.kc@gmail.com>
2022-05-24 18:51:01 -0400

    first commit

---
+++ server/server.js
@@ -0,0 +1,48 @@
+const express = require("express");
+const { ApolloServer } = require("apollo-server-express");
+const { authMiddleware } = require("./utils/auth");
+
+const path = require("path");
+const db = require("./config/connection");
+const routes = require("./routes");
+
+//import our typeDefs and resolvers
+const { typeDefs, resolvers } = require("./schemas");
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+//create new Apollo server and pass in our schema data
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: authMiddleware,
+});
+
+//integrate our Apollo server with Express application as middleware
+server.applyMiddleware({ app });
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// if we're in production, serve client/build as static assets
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../client/build")));
+}
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+});
+
+db.once("open", () => {
+  app.listen(PORT, () => {
+    console.log(`📚 API server running on port ${PORT}!`);
+    console.log(
+      `🚀 Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`
+    );
+  });
+});
+
+process.on("uncaughtException", function (err) {
+  console.log("Caught exception: " + err);
+});
